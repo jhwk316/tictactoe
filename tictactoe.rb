a5f16995f7c50648c4b39a147a705495390ba000
Jeff Andrews <jhawknut@gmail.com>
2022-09-26 21:04:21 -0500

    add user input

---
+++ tictactoe.rb
@@ -0,0 +1,13 @@
+## Input for Player 1 name
+puts ""
+puts "Type in the name for player 1"
+
+puts "Welcome #{player1.name}, you are playing as X"
+puts ""
+
+##  Input for Player 2 name
+puts ""
+puts "Type in the name for player 2"
+
+puts "Welcome #{player2.name}, you are playing as O"
+puts ""
